c0095d4c4aa03e861f2c07350dd2f4012c70ab70
bmxshithead <67148946+Llamaraa@users.noreply.github.com>
2024-05-19 00:00:11 +0100

    Added the Python script

---
+++ numberlist.py
@@ -0,0 +1,3 @@
+for number in range(0, 5): # loops through numbers in a range from 0 to 4 (inclusive)
+    print(number) # prints out the current number that's in the loop
+input("Press ENTER to exit this program.") # exits the program when ENTER is pressed	
